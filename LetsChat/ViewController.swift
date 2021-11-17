8de13db9958709586bb53e9dadb12d33719d0b65
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-17 14:24:21 +0900

    Initial Commit

---
+++ LetsChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsChat
+//
+//  Created by Mai Uchida on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
